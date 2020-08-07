aa464be278425ddf67ed00165a9e186c65f0273b
Guohan Lu <lguohan@gmail.com>
2020-08-07 11:58:50 +0000

    port channel
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- config/portchannel.py
+++ config/portchannel.py
@@ -23,14 +23,14 @@ def add_portchannel(db, portchannel_name, min_links, fallback):
         fvs['min_links'] = str(min_links)
     if fallback != 'false':
         fvs['fallback'] = 'true'
-    db.cdb.set_entry('PORTCHANNEL', portchannel_name, fvs)
+    db.cfgdb.set_entry('PORTCHANNEL', portchannel_name, fvs)
 
 @portchannel.command('del')
 @click.argument('portchannel_name', metavar='<portchannel_name>', required=True)
 @pass_db
 def remove_portchannel(db, portchannel_name):
     """Remove port channel"""
-    db.cdb.set_entry('PORTCHANNEL', portchannel_name, None)
+    db.cfgdb.set_entry('PORTCHANNEL', portchannel_name, None)
 
 @portchannel.group(cls=AbbreviationGroup, name='member')
 def portchannel_member():
@@ -43,9 +43,9 @@ def portchannel_member():
 @pass_db
 def add_portchannel_member(db, portchannel_name, port_name):
     """Add member to port channel"""
-    if interface_is_mirror_dst_port(db.cdb, port_name):
+    if is_interface_mirror_dst_port(db.cfgdb, port_name):
         ctx.fail("{} is configured as mirror destination port".format(port_name))
-    db.cdb.set_entry('PORTCHANNEL_MEMBER', (portchannel_name, port_name),
+    db.cfgdb.set_entry('PORTCHANNEL_MEMBER', (portchannel_name, port_name),
             {'NULL': 'NULL'})
 
 @portchannel_member.command('del')
@@ -54,5 +54,5 @@ def add_portchannel_member(db, portchannel_name, port_name):
 @pass_db
 def del_portchannel_member(db, portchannel_name, port_name):
     """Remove member from portchannel"""
-    db.cdb.set_entry('PORTCHANNEL_MEMBER', (portchannel_name, port_name), None)
-    db.cdb.set_entry('PORTCHANNEL_MEMBER', portchannel_name + '|' + port_name, None)
+    db.cfgdb.set_entry('PORTCHANNEL_MEMBER', (portchannel_name, port_name), None)
+    db.cfgdb.set_entry('PORTCHANNEL_MEMBER', portchannel_name + '|' + port_name, None)
--- utilities_common/cli.py
+++ utilities_common/cli.py
@@ -41,3 +41,46 @@ class AbbreviationGroup(click.Group):
                 return click.Group.get_command(self, ctx, shortest)
 
             ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
+
+def interface_is_in_vlan(vlan_member_table, interface_name):
+    """ Check if an interface  is in a vlan """
+    for _,intf in vlan_member_table.keys():
+        if intf == interface_name:
+            return True
+
+    return False
+
+def interface_is_in_portchannel(portchannel_member_table, interface_name):
+    """ Check if an interface is part of portchannel """
+    for _,intf in portchannel_member_table.keys():
+        if intf == interface_name:
+            return True
+
+    return False
+
+def interface_is_router_port(interface_table, interface_name):
+    """ Check if an interface has router config """
+    for intf in interface_table.keys():
+        if (interface_name == intf[0]):
+            return True
+
+    return False
+
+def is_interface_mirror_dst_port(config_db, interface_name):
+    """ Check if port is already configured as mirror destination port """
+    mirror_table = config_db.get_table('MIRROR_SESSION')
+    for _,v in mirror_table.items():
+        if 'dst_port' in v and v['dst_port'] == interface_name:
+            return True
+
+    return False
+
+def interface_has_mirror_config(mirror_table, interface_name):
+    """ Check if port is already configured with mirror config """
+    for _,v in mirror_table.items():
+        if 'src_port' in v and v['src_port'] == interface_name:
+            return True
+        if 'dst_port' in v and v['dst_port'] == interface_name:
+            return True
+
+    return False
